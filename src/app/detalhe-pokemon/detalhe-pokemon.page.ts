f607b68f7c6deae81ccec1b534f9c55b981e0d6f
Vitória Ketillin Fusco <vitoriaketillin@hotmail.com>
2021-08-09 00:18:09 -0300

    v1.3 Página detalhes pokemon

--- src/app/detalhe-pokemon/detalhe-pokemon.page.ts
+++ src/app/detalhe-pokemon/detalhe-pokemon.page.ts
@@ -7,9 +7,47 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DetalhePokemonPage implements OnInit {
 
+  pokemon = {
+    nome: 'Bulbasaur',
+    numero: '001',
+    descricao: 'Há uma semente de planta em suas costas desde o dia em que o Pokémon nasceu. A semente cresce lentamente.',
+    versoes: 2,
+    altura: 0.7,
+    peso: 6.9,
+    categoria: 'Semente',
+    habilidades: 'Overgrow',
+    genero: 2,
+    tipo: ['Grass', 'Poison'],
+    fraquezas: ['Fire', 'Psychic', 'Flying', 'Ice'],
+    status: {hp: 3, attack: 3, defense: 3, specialAttack: 4, specialDefense: 4, speed: 3},
+    foto: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/001.png',
+    evolucoes: [
+      {
+        fotoEvolucao: 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/001.png',
+        nomeEvolucao: 'Bulbasaur',
+        numeroEvolucao: '001',
+        tipoEvolucao: ['Grass', 'Poison']
+      },{
+        fotoEvolucao: 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/002.png',
+        nomeEvolucao: 'Ivysaur',
+        numeroEvolucao: '002',
+        tipoEvolucao: ['Grass', 'Poison']
+      },{
+        fotoEvolucao: 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/003.png',
+        nomeEvolucao: 'Venusaur',
+        numeroEvolucao: '003',
+        tipoEvolucao: ['Grass', 'Poison']
+      }
+      ]
+  };
+
   constructor() { }
 
   ngOnInit() {
   }
 
 }
+
+
+
+
